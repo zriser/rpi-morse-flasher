79d02bb7928e46c4cf968742bcc33e6129306f2d
zriser <zachary.riser@gmail.com>
2019-10-28 20:26:44 -0400

    Create morse.py

---
+++ morse.py
@@ -0,0 +1,60 @@
+import RPi.GPIO as GPIO
+import time
+
+try:
+	while True:
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(18, GPIO.OUT)
+
+		def blinkdot():
+			GPIO.output(18, GPIO.HIGH)
+			time.sleep(0.5)
+			GPIO.output(18, GPIO.LOW)
+			time.sleep(0.5)
+
+		def blinkdash():
+			GPIO.output(18, GPIO.HIGH)
+			time.sleep(1.5)
+			GPIO.output(18, GPIO.LOW)
+			time.sleep(0.5)
+
+		def space():
+			time.sleep(1.5)
+
+		morse = {'A': '.-',     'B': '-...',   'C': '-.-.',
+		        'D': '-..',    'E': '.',      'F': '..-.',
+		        'G': '--.',    'H': '....',   'I': '..',
+		        'J': '.---',   'K': '-.-',    'L': '.-..',
+		        'M': '--',     'N': '-.',     'O': '---',
+		        'P': '.--.',   'Q': '--.-',   'R': '.-.',
+		        'S': '...',    'T': '-',      'U': '..-',
+		        'V': '...-',   'W': '.--',    'X': '-..-',
+		        'Y': '-.--',   'Z': '--..',
+		        '0': '-----',  '1': '.----',  '2': '..---',
+		        '3': '...--',  '4': '....-',  '5': '.....',
+		        '6': '-....',  '7': '--...',  '8': '---..',
+		        '9': '----.',' ':' '
+		        }
+
+		msg = 'happy halloween asymmetrik'
+		msg = msg.upper()
+
+		for i in msg:
+			blink = morse[i]
+			print(blink)
+
+			for z in blink:
+				if z == '.':
+					blinkdot()
+				elif z == '-':
+					blinkdash()
+				else:
+					space()
+
+		time.sleep(2)
+
+except KeyboardInterrupt:
+	print('cleanup!')
+
+finally:
+	GPIO.cleanup()
